21315055cfac9c7687c99f1b21100f5b5396e99e
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-01-19 10:42:55 +0100

    Refactor DB schema: replace categorie with programme
    Removed the 'categorie' table and introduced a new 'programme' table. Updated 'exercice' and 'seances' tables to reference 'programme' instead of 'categorie', including all related foreign keys, indexes, and data. Adjusted sample data and performance records accordingly. Also deleted the .env file.

--- surcharge-progressive.sql
+++ surcharge-progressive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 19 jan. 2026 à 00:39
+-- Généré le : lun. 19 jan. 2026 à 10:41
 -- Version du serveur : 8.0.39
 -- Version de PHP : 8.2.12
 
@@ -24,31 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
---
-
-CREATE TABLE `categorie` (
-  `id` int UNSIGNED NOT NULL,
-  `libelle` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `categorie`
---
-
-INSERT INTO `categorie` (`id`, `libelle`) VALUES
-(1, 'Upper'),
-(2, 'Lower');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `exercice`
 --
 
 CREATE TABLE `exercice` (
   `id` int UNSIGNED NOT NULL,
-  `idCategorie` int UNSIGNED DEFAULT NULL,
+  `idProgramme` int UNSIGNED DEFAULT NULL,
   `libelle` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `charge` float DEFAULT NULL,
   `nbSeries` int DEFAULT NULL,
@@ -60,7 +41,7 @@ CREATE TABLE `exercice` (
 -- Déchargement des données de la table `exercice`
 --
 
-INSERT INTO `exercice` (`id`, `idCategorie`, `libelle`, `charge`, `nbSeries`, `estActif`, `ordre`) VALUES
+INSERT INTO `exercice` (`id`, `idProgramme`, `libelle`, `charge`, `nbSeries`, `estActif`, `ordre`) VALUES
 (1, 1, 'Développé couché', 65, 3, 1, 1),
 (2, 1, 'Tirage vertical', 65, 3, 1, 2),
 (3, 1, 'Développé incliné', 42.5, 3, 1, 3),
@@ -99,9 +80,9 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(88, '2026-01-18-000001', 'App\\Database\\Migrations\\CreateBase', 'default', 'App', 1768779572, 1),
-(89, '2026-01-18-000002', 'App\\Database\\Migrations\\CreateSeances', 'default', 'App', 1768779572, 1),
-(90, '2026-01-18-000003', 'App\\Database\\Migrations\\CreatePerformances', 'default', 'App', 1768779572, 1);
+(7, '2026-01-18-000001', 'App\\Database\\Migrations\\CreateBase', 'default', 'App', 1768815672, 1),
+(8, '2026-01-18-000002', 'App\\Database\\Migrations\\CreateSeances', 'default', 'App', 1768815672, 1),
+(9, '2026-01-18-000003', 'App\\Database\\Migrations\\CreatePerformances', 'default', 'App', 1768815673, 1);
 
 -- --------------------------------------------------------
 
@@ -123,54 +104,74 @@ CREATE TABLE `performances` (
 --
 
 INSERT INTO `performances` (`id`, `idSeance`, `idExercice`, `numero_serie`, `reps`, `poids_effectif`) VALUES
-(1, 1, 1, 1, 12, 65),
-(2, 1, 1, 2, 9, 65),
-(3, 1, 1, 3, 10, 65),
-(4, 1, 2, 1, 9, 65),
-(5, 1, 2, 2, 9, 65),
-(6, 1, 2, 3, 6, 65),
-(7, 1, 3, 1, 9, 42.5),
-(8, 1, 3, 2, 9, 42.5),
-(9, 1, 3, 3, 7, 42.5),
-(10, 1, 4, 1, 8, 32.5),
+(1, 1, 1, 1, 11, 65),
+(2, 1, 1, 2, 10, 65),
+(3, 1, 1, 3, 7, 65),
+(4, 1, 2, 1, 11, 65),
+(5, 1, 2, 2, 8, 65),
+(6, 1, 2, 3, 8, 65),
+(7, 1, 3, 1, 8, 42.5),
+(8, 1, 3, 2, 10, 42.5),
+(9, 1, 3, 3, 9, 42.5),
+(10, 1, 4, 1, 10, 32.5),
 (11, 1, 4, 2, 9, 32.5),
-(12, 1, 4, 3, 6, 32.5),
-(13, 1, 5, 1, 11, 12),
+(12, 1, 4, 3, 7, 32.5),
+(13, 1, 5, 1, 8, 12),
 (14, 1, 5, 2, 8, 12),
-(15, 1, 5, 3, 8, 12),
-(16, 1, 6, 1, 11, 12),
-(17, 1, 6, 2, 9, 12),
-(18, 1, 6, 3, 6, 12),
-(19, 1, 7, 1, 9, 10),
+(15, 1, 5, 3, 9, 12),
+(16, 1, 6, 1, 12, 12),
+(17, 1, 6, 2, 12, 12),
+(18, 1, 6, 3, 10, 12),
+(19, 1, 7, 1, 10, 10),
 (20, 1, 7, 2, 10, 10),
-(21, 1, 7, 3, 9, 10),
-(22, 1, 8, 1, 8, 27.5),
-(23, 1, 8, 2, 12, 27.5),
-(24, 1, 8, 3, 6, 27.5),
+(21, 1, 7, 3, 10, 10),
+(22, 1, 8, 1, 12, 27.5),
+(23, 1, 8, 2, 8, 27.5),
+(24, 1, 8, 3, 7, 27.5),
 (25, 1, 9, 1, 9, 12),
-(26, 1, 9, 2, 12, 12),
-(27, 1, 9, 3, 7, 12),
-(28, 1, 10, 1, 10, 14),
+(26, 1, 9, 2, 9, 12),
+(27, 1, 9, 3, 9, 12),
+(28, 1, 10, 1, 12, 14),
 (29, 1, 10, 2, 8, 14),
-(30, 1, 10, 3, 6, 14),
-(31, 1, 11, 1, 12, 22.5),
-(32, 1, 11, 2, 12, 22.5),
-(33, 1, 11, 3, 9, 22.5),
-(34, 1, 12, 1, 9, 17.5),
+(30, 1, 10, 3, 7, 14),
+(31, 1, 11, 1, 8, 22.5),
+(32, 1, 11, 2, 10, 22.5),
+(33, 1, 11, 3, 10, 22.5),
+(34, 1, 12, 1, 11, 17.5),
 (35, 1, 12, 2, 12, 17.5),
-(36, 1, 12, 3, 7, 17.5),
-(37, 2, 13, 1, 12, 65),
+(36, 1, 12, 3, 8, 17.5),
+(37, 2, 13, 1, 10, 65),
 (38, 2, 13, 2, 10, 65),
-(39, 2, 13, 3, 7, 65),
-(40, 2, 14, 1, 10, 60),
-(41, 2, 14, 2, 11, 60),
-(42, 2, 14, 3, 10, 60),
-(43, 2, 15, 1, 10, 67.5),
-(44, 2, 15, 2, 9, 67.5),
-(45, 2, 15, 3, 6, 67.5),
-(46, 2, 16, 1, 11, 130),
-(47, 2, 16, 2, 12, 130),
-(48, 2, 16, 3, 8, 130);
+(39, 2, 13, 3, 8, 65),
+(40, 2, 14, 1, 8, 60),
+(41, 2, 14, 2, 8, 60),
+(42, 2, 14, 3, 8, 60),
+(43, 2, 15, 1, 11, 67.5),
+(44, 2, 15, 2, 8, 67.5),
+(45, 2, 15, 3, 10, 67.5),
+(46, 2, 16, 1, 9, 130),
+(47, 2, 16, 2, 11, 130),
+(48, 2, 16, 3, 6, 130);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `programme`
+--
+
+CREATE TABLE `programme` (
+  `id` int UNSIGNED NOT NULL,
+  `libelle` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `estActif` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `programme`
+--
+
+INSERT INTO `programme` (`id`, `libelle`, `estActif`) VALUES
+(1, 'Upper', 1),
+(2, 'Lower', 1);
 
 -- --------------------------------------------------------
 
@@ -180,7 +181,7 @@ INSERT INTO `performances` (`id`, `idSeance`, `idExercice`, `numero_serie`, `rep
 
 CREATE TABLE `seances` (
   `id` int UNSIGNED NOT NULL,
-  `idCategorie` int UNSIGNED DEFAULT NULL,
+  `idProgramme` int UNSIGNED DEFAULT NULL,
   `date_seance` date NOT NULL,
   `status` enum('en_cours','fini') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'en_cours'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -189,7 +190,7 @@ CREATE TABLE `seances` (
 -- Déchargement des données de la table `seances`
 --
 
-INSERT INTO `seances` (`id`, `idCategorie`, `date_seance`, `status`) VALUES
+INSERT INTO `seances` (`id`, `idProgramme`, `date_seance`, `status`) VALUES
 (1, 1, '2026-01-17', 'fini'),
 (2, 2, '2026-01-15', 'fini');
 
@@ -198,17 +199,11 @@ INSERT INTO `seances` (`id`, `idCategorie`, `date_seance`, `status`) VALUES
 --
 
 --
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Index pour la table `exercice`
 --
 ALTER TABLE `exercice`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `exercice_idCategorie_foreign` (`idCategorie`);
+  ADD KEY `exercice_idProgramme_foreign` (`idProgramme`);
 
 --
 -- Index pour la table `migrations`
@@ -225,21 +220,21 @@ ALTER TABLE `performances`
   ADD KEY `performances_idExercice_foreign` (`idExercice`);
 
 --
+-- Index pour la table `programme`
+--
+ALTER TABLE `programme`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `seances`
 --
 ALTER TABLE `seances`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `seances_idCategorie_foreign` (`idCategorie`);
+  ADD KEY `seances_idProgramme_foreign` (`idProgramme`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `exercice`
@@ -251,13 +246,19 @@ ALTER TABLE `exercice`
 -- AUTO_INCREMENT pour la table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `performances`
 --
 ALTER TABLE `performances`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT pour la table `programme`
+--
+ALTER TABLE `programme`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `seances`
@@ -273,7 +274,7 @@ ALTER TABLE `seances`
 -- Contraintes pour la table `exercice`
 --
 ALTER TABLE `exercice`
-  ADD CONSTRAINT `exercice_idCategorie_foreign` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `exercice_idProgramme_foreign` FOREIGN KEY (`idProgramme`) REFERENCES `programme` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `performances`
@@ -286,7 +287,7 @@ ALTER TABLE `performances`
 -- Contraintes pour la table `seances`
 --
 ALTER TABLE `seances`
-  ADD CONSTRAINT `seances_idCategorie_foreign` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `seances_idProgramme_foreign` FOREIGN KEY (`idProgramme`) REFERENCES `programme` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
